2c907815823edc84672052cb8e2be284a1c4ed69
Montasir hasan <montasirhasan65@gmail.com>
2025-08-07 13:33:51 +0600

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,20 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import Home from './pages/Home';
-import Referral from './pages/Referral';
-import Mission from './pages/Mission';
-import History from './pages/History';
+import About from './pages/About';
+import Ref from './pages/Ref';
+import Earn from './pages/Earn';
 import Wallet from './pages/Wallet'; // ✅ Make sure this path is correct
-
 import Footer from './Components/Footer'; // ✅ Your footer path
 
 const AppRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ref" element={<Referral />} />
-        <Route path="/earn" element={<Mission />} />
-        <Route path="/about" element={<History />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/ref" element={<Ref />} />
+      <Route path="/earn" element={<Earn />} />
         <Route path="/wallet" element={<Wallet />} /> {/* ✅ Wallet page */}
       </Routes>
 
@@ -25,3 +24,4 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
+

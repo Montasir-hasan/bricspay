9ec597cd1d48910f1b7fd425d93726d4598d6248
Montasir hasan <montasirhasan65@gmail.com>
2025-08-07 15:33:27 +0600

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,11 +1,11 @@
+// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDWyrP8xniw9kUJUBVCdhEIRs3Mw-BATj0",
   authDomain: "brics-pay.firebaseapp.com",
-  databaseURL: "https://brics-pay-default-rtdb.firebaseio.com",
   projectId: "brics-pay",
   storageBucket: "brics-pay.firebasestorage.app",
   messagingSenderId: "516647996624",
@@ -13,9 +13,11 @@ const firebaseConfig = {
   measurementId: "G-XPLKCMKF79"
 };
 
-
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore database
 const db = getFirestore(app);
 
-export  {db};
-
+// Export Firestore instance for use in your app
+export { db };

dcda7b5921fad52bb196362d392bd6c335f7ca5b
Montasir hasan <montasirhasan65@gmail.com>
2025-08-07 16:54:02 +0600

    Update Ref.jsx

--- src/pages/Ref.jsx
+++ src/pages/Ref.jsx
@@ -2,7 +2,7 @@ import Footer from "../Components/Footer";
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BackButton from '../Components/BackButton';
-import { db } from '../database/firebase.js';
+import { db } from '../database/firebase';
 import { collection, getDocs, doc, updateDoc, increment, getDoc } from "@firebase/firestore";
 import { useTonCoin } from '../Components/Context/TonCoinContext.jsx';
 import { FaCheckCircle } from 'react-icons/fa';
@@ -219,3 +219,4 @@ const Ref = () => {
 };
 
 export default Ref;
+

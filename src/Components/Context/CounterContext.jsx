40247863d107c5450510eddc708e7c60287aabca
Montasir hasan <montasirhasan65@gmail.com>
2025-08-08 10:44:59 +0600

    Update CounterContext.jsx

--- src/Components/Context/CounterContext.jsx
+++ src/Components/Context/CounterContext.jsx
@@ -1,4 +1,4 @@
-import  { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const CounterContext = createContext();
 
@@ -6,11 +6,23 @@ export const useCounter = () => {
   return useContext(CounterContext);
 };
 
+// This provider will now be managed by App.jsx
 export const CounterProvider = ({ children }) => {
   const [counter, setCounter] = useState(0.000000000);
+  const [minerSpeed, setMinerSpeed] = useState(2);
+  const [telegramUserId, setTelegramUserId] = useState(null);
+
+  const value = {
+    counter,
+    setCounter,
+    minerSpeed,
+    setMinerSpeed,
+    telegramUserId,
+    setTelegramUserId,
+  };
 
   return (
-    <CounterContext.Provider value={{ counter, setCounter }}>
+    <CounterContext.Provider value={value}>
       {children}
     </CounterContext.Provider>
   );
